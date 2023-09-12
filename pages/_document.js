73641ba1d81fcd241f12ad428155020adf4b0f77
Burak Gündüz <burakkgunduz21@gmail.com>
2023-09-12 13:30:15 +0300

    add meticulus AI

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {(process.env.NODE_ENV === "development" ||
+          process.env.NEXT_PUBLIC_VERCEL_ENV === "production") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="VlFg2Ivc3gEpBuWzaWKL3B4clZbv3RYchHPGIGMC"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
